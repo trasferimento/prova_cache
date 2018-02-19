77ec0e8ccc41609fb8646b8e016f16e255c42fb1
AF <antonio.farabegoli@skytv.it>
2018-02-19 17:39:38 +0100

    Aggiungere i file di progetto.

---
+++ prova_cache_redis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prova_cache_redis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
